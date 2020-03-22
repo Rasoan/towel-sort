63fc188bc7e974612c061f9fd85f1fa1093c4e35
Araik Rasoyan <araikrasoian@gmail.com>
2020-03-22 17:15:14 +0300

    feat: star towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+ if(  !(Array.isArray(matrix) && matrix.length > 0) )
   return [];
+
+  let flag = true;
+ 
+  let sort_matrix = [];
+
+ matrix.forEach(element => {
+    
+   if(flag == true){ 
+    
+   sort_matrix.push( ...element.sort(function(a, b) {
+     return a - b;
+    }));
+   }
+   else {
+    sort_matrix.push( ...element.sort(function(a, b) {
+        return b - a;
+       }));
+   }
+  flag = !flag;
+ });
+
+  return sort_matrix;
 }
+
+
+
+
+
+
